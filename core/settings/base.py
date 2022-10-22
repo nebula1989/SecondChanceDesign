db3058128588fdc4fbf11849c4d65691d9eb05e0
nebula1989 <benwalterscoding@gmail.com>
2022-10-21 23:30:15 -0700

    updated settings again

--- core/settings/base.py
+++ core/settings/base.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'apps/../../apps/static'),
+    os.path.join(CORE_DIR, '../apps/static'),
 )
 
 #############################################################
