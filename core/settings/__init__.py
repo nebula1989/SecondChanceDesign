05f2d5b29eb9f1fce002900057aafc4bf28a405e
nebula1989 <benwalterscoding@gmail.com>
2023-12-09 18:38:47 -0500

    added os detection

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,6 +1,28 @@
 import json
+import platform
 
-CONFIG_FILE = '/etc/secondchance_config.json'
+def detect_os():
+    system = platform.system()
+    
+    if system == 'Windows':
+        # Do something for Windows
+        config_file = "C:\\Users\\benwa\\etc\\secondchance_config.json"
+        # Your Windows-specific code here
+        
+    elif system == 'Linux':
+        distro = platform.linux_distribution()
+        if 'Ubuntu' in distro:
+            # Do something for Ubuntu
+            config_file = '/etc/secondchance_config.json'
+            # Your Ubuntu-specific code here
+        
+    else:
+        print("Unknown or unsupported operating system")
+
+    return config_file
+
+
+CONFIG_FILE = detect_os()
 
 try:
     with open(CONFIG_FILE) as config_file:
