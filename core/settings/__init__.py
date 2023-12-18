b62c25cabb1a39c2a6333738131c99a310fcb58f
nebula1989 <benwalterscoding@gmail.com>
2023-12-17 20:16:57 -0500

    wtf is goingin on

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,25 +1,19 @@
 import json
 import platform
-from distro import linux_distribution
 
-def detect_os():
-    system = platform.system()
+system = platform.system()
+
+if system == 'Windows':
+    # Do something for Windows
+    config_file = "C:\\Users\\benwa\\etc\\secondchance_config.json"
+    # Your Windows-specific code here
+        
+else:
+    config_file = '/etc/secondchance_config.json'
     
-    if system == 'Windows':
-        # Do something for Windows
-        config_file = "C:\\Users\\benwa\\etc\\secondchance_config.json"
-        # Your Windows-specific code here
-            
-    else:
-        config_file = '/etc/secondchance_config.json'
-
-    return config_file
-
-
-CONFIG_FILE = detect_os()
 
 try:
-    with open(CONFIG_FILE) as config_file:
+    with open(config_file):
         config = json.load(config_file)
         config['PROD']
     from .prod import *
