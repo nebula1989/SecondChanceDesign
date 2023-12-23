6c18d49ce09853feca538ebbad1e318d7d2cef84
nebula1989 <benwalterscoding@gmail.com>
2023-12-22 22:12:20 -0500

    when delete photo in admin, deletes photo from media too

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='media/uploads/')
     description = models.TextField()
+
+    def delete(self, *args, **kwargs):
+        # Delete the file from the filesystem when the object is deleted
+        if os.path.isfile(self.image.path):
+            os.remove(self.image.path)
+        super(Photo, self).delete(*args, **kwargs)
 
     def __str__(self):
         return self.title
