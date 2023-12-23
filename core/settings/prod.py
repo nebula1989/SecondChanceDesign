d6a21bb4a107634910568d43d0b83bc967389693
nebula1989 <benwalterscoding@gmail.com>
2023-12-23 18:58:16 -0500

    testing debug on

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,6 +2,6 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['http://secondchancedesign.com', 'secondchance.design', 'www.secondchance.design', 'http://www.secondchance.design', 'http://www.secondchance.design/admin', '54.90.219.106']
 CSRF_TRUSTED_ORIGINS = ['http://secondchance.design', 'http://www.secondchance.design', 'http://www.secondchance.design/admin']
