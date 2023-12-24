26ba6e0a64c708dc330ed282ba99b48dc5a51eab
nebula1989 <benwalterscoding@gmail.com>
2023-12-24 10:00:49 -0500

    reverted photo deletion, not working...

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -1,23 +1,6 @@
 from django.db import models
-import os
 
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='media/uploads/')
     description = models.TextField()
-
-    def delete(self, *args, **kwargs):
-        # Get the image path
-        image_path = self.image.path
-        
-        # Ensure the file exists before attempting deletion
-        if os.path.exists(image_path):
-            try:
-                # Attempt to remove the file
-                os.remove(image_path)
-            except Exception as e:
-                # Handle any exceptions during file deletion
-                print(f"Error deleting file: {str(e)}")
-
-        # Call the parent delete method after handling the file
-        super(Photo, self).delete(*args, **kwargs)
