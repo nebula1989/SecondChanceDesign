9015d7c7dd07b2bdc3f0c856fe313215c372a025
nebula1989 <benwalterscoding@gmail.com>
2023-12-23 19:51:25 -0500

    delete from media when delete in admin

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 import os
 
-# Create your models here.
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='media/uploads/')
     description = models.TextField()
 
     def delete(self, *args, **kwargs):
-        # Delete the file from the filesystem when the object is deleted
-        if os.path.isfile(self.image.path):
-            os.remove(self.image.path)
-        super(Photo, self).delete(*args, **kwargs)
+        # Get the image path
+        image_path = self.image.path
+        
+        # Ensure the file exists before attempting deletion
+        if os.path.exists(image_path):
+            try:
+                # Attempt to remove the file
+                os.remove(image_path)
+            except Exception as e:
+                # Handle any exceptions during file deletion
+                print(f"Error deleting file: {str(e)}")
 
-    def __str__(self):
-        return self.title
+        # Call the parent delete method after handling the file
+        super(Photo, self).delete(*args, **kwargs)
