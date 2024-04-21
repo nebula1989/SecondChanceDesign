317a668a28fc0416834d72bfdcf2aee6ef4489b5
nebula1989 <benwalterscoding@gmail.com>
2024-04-21 00:41:56 -0400

    updated user admin privs

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Photo(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='media/uploads/')
     description = models.TextField()
